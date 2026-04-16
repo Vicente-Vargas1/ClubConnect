bb0be09abdddd3250bd9b6c6948d91486a5c8754
Vicente Vargas <vincentvargas300@gmail.com>
2026-04-15 20:48:28 -0400

    fixed profile view

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -155,3 +155,21 @@ export const checkAuth = (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+/* =========================
+   GET USER BY ID
+========================= */
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.log("Error in getUserById:", error.message);
+    res.status(500).json({ message: "Server error" });
+  }
+};
